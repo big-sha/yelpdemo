eb6dab43147fd337961946669a0b49aec97e338a
perkins1 <smperk71@gmail.com>
2015-06-26 13:07:42 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: 'Ho Yi', address: '200 Fake St, Wilkes-Barre PA 18702', phone: '570-270-1521', website: 'www.yoyi.com')
+Restaurant.create(name: 'Red Lobster', address: '300 Fake St, Wilkes-Barre PA 18702', phone: '570-270-1522', website: 'www.redlobster.com')
+Restaurant.create(name: 'China Jade', address: '1 Carbon St, Weatherly PA 18255', phone: '570-427-8600', website: 'www.chinajade.com')
