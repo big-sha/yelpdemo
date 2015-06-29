f15d936dc5fb4d5bbb006abf1ea80c249e706fdd
perkins1 <smperk71@gmail.com>
2015-06-29 14:36:37 -0400

    Add custom css

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,12 +9,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   #storage :file
   if Rails.env.production?
-  storage :fog
+    storage :fog
   else
     storage :file
   end
 
   process :resize_to_fit => [200, 300]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
