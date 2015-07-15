817d8ce55d6b2bf789498ff84a751f634e6ecda5
perkins1 <smperk71@gmail.com>
2015-07-15 16:07:49 -0400

    Add bootstrap styling to reviews show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Yelpdemo::Application.routes.draw do
-  resources :reviews, except: [:show, :index]
-
+  
   devise_for :users
   resources :restaurants do
     resources :reviews, expect: [:show, :index]
